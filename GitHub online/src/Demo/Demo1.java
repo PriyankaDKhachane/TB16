41f5aee686e4a749c7f225a9a668be1102abcb4d
USER <USER@DESKTOP-5QRTC1T>
2022-08-02 15:10:55 +0530

    by testing1

--- GitHub online/src/Demo/Demo1.java
+++ GitHub online/src/Demo/Demo1.java
@@ -8,7 +8,7 @@ public class Demo1
 	}
 	public static void main(String[] args) 
 	{
-		System.out.println("hello");
+		//System.out.println("hello");
 		System.out.println("git");
 		System.out.println("Git by cjc1");
 	}
---
+++ GitHub online/src/Demo/Task_Git.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Task_Git 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("gitHub...01-08-22");
+		
+	}
+
+}

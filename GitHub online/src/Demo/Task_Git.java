fcd42335a3005db091ba3623de4ea9c4444970c5
PriyankaDKhachane <110393166+PriyankaDKhachane@users.noreply.github.com>
2022-08-02 15:13:30 +0530

    change by team

--- GitHub online/src/Demo/Task_Git.java
+++ GitHub online/src/Demo/Task_Git.java
@@ -2,6 +2,10 @@ package Demo;
 
 public class Task_Git 
 {
+	public void m2()
+	{
+		System.out.println("m2...Start");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("gitHub...01-08-22");
